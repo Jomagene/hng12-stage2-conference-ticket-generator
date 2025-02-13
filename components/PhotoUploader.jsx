b4ee836b9295e565a156acc0696d6089e5281381
Semjo <semjomagene@gmail.com>
2025-02-13 04:01:21 +0200

    Add drag and drop file upload option

--- components/PhotoUploader.jsx
+++ components/PhotoUploader.jsx
@@ -1,6 +1,34 @@
+'use client';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const PhotoUploader = () => {
+  const [image, setImage] = useState(null);
+  const [dragOver, setDragOver] = useState(false);
+
+  const handleImageUpload = (file) => {
+    if (file) setImage(URL.createObjectURL(file));
+  };
+
+  const handleFileChange = (event) => {
+    handleImageUpload(event.target.files[0]);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    setDragOver(true);
+  };
+
+  const handleDragLeave = () => {
+    setDragOver(false);
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setDragOver(false);
+    handleImageUpload(event.dataTransfer.files[0]);
+  };
+
   return (
     <div className="p-6 pb-12 rounded-3xl bg-[#052228] flex flex-col border border-[#07373F] gap-8">
       <p>Upload Profile Photo</p>
@@ -11,17 +39,33 @@ const PhotoUploader = () => {
           name="profile-pic"
           id="profile-pic"
           className="hidden"
+          accept="image/*"
+          onChange={handleFileChange}
         />
         <label
           htmlFor="profile-pic"
-          className="border-4 border-[#24A0B580] rounded-[32px] bg-[#0E464F] w-60 h-60 p-6 flex flex-col gap-4 justify-center items-center relative top-[-220px] mx-auto">
-          <Image
-            src="/cloud-download.png"
-            height={32}
-            width={32}
-            alt="cloud download icon"
-          />
-          <p className="text-center">Drag & drop or click to Upload</p>
+          className="border-4 border-[#24A0B580] rounded-[32px] bg-[#0E464F] w-60 h-60 overflow-hidden flex flex-col gap-4 justify-center items-center relative top-[-220px] mx-auto"
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}>
+          {image ? (
+            <Image
+              src={image}
+              height={264}
+              width={264}
+              alt="Uploaded preview"
+            />
+          ) : (
+            <>
+              <Image
+                src="/cloud-download.png"
+                height={32}
+                width={32}
+                alt="cloud download icon"
+              />
+              <p className="text-center">Drag & drop or click to Upload</p>
+            </>
+          )}
         </label>
       </div>
     </div>
